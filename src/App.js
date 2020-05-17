a210ff291ced6921354aeb9ada35fe432ec95dfe
Xalvic <puthukkuddymilan@gmail.com>
2020-05-17 16:02:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='head'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='1000'
+          height='275'
+          viewBox='0 0 803 275'
         >
-          Learn React
-        </a>
-      </header>
+          <defs>
+            <filter
+              id='Line_3'
+              x='0'
+              y='140'
+              width='818'
+              height='53'
+              filterUnits='userSpaceOnUse'
+            >
+              <feOffset input='SourceAlpha' />
+              <feGaussianBlur stdDeviation='7.5' result='blur' />
+              <feFlood flood-color='#d35400' />
+              <feComposite operator='in' in2='blur' />
+              <feComposite in='SourceGraphic' />
+            </filter>
+          </defs>
+          <g id='Group_1' data-name='Group 1' transform='translate(-228 -75)'>
+            <text
+              id='CSS_Test_Labs'
+              data-name='CSS Test Labs'
+              class='cls-1'
+              transform='translate(229 76)'
+            >
+              <tspan id='css' x='0' y='140'>
+                CSS
+              </tspan>
+              <tspan id='test' x='260'>
+                Test
+              </tspan>
+              <tspan id='labs'>Labs</tspan>
+            </text>
+            <g class='cls-3' transform='matrix(1, 0, 0, 1, 209, 75)'>
+              <line
+                id='Line_3-2'
+                data-name='Line 3'
+                class='cls-2'
+                x2='765'
+                transform='translate(26.5 166.5)'
+              />
+            </g>
+          </g>
+        </svg>
+      </div>
     </div>
   );
 }
